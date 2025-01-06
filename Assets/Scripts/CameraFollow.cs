3f2c5fcc24c9710bb4637069a7acc91b3f5cc9ae
Anjana Modugu <anjanamodugu@gmail.com>
2025-01-07 01:30:40 +0530

    Added the player movement and camera follow

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,16 @@
+using System;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform player;
+    Vector3 offset;
+    private void Start(){
+        offset = transform.position - player.position;
+    }
+    private void Update(){
+        Vector3 targetPos = player.position + offset;
+        targetPos.x = 0;
+        transform.position = targetPos;
+    }
+}
